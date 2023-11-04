bdfd19456f31487d14e21447b054fa4729b6980d
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-11-04 18:07:45 -0300

    feat(mui): add custom theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import theme from "./themes/theme";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/themes/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2196f3",
+    },
+  },
+});
+
+export default theme;
