010c5739f5ee1fdc753f868c1c94ddedf0354e95
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-11-06 12:07:08 -0300

    feat(milestones): :sparkles: add milestones section

--- src/components/Milestones.jsx
+++ src/components/Milestones.jsx
@@ -1,9 +1,46 @@
-
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
 
 const Milestones = () => {
-  return (
-    <div>Milestones</div>
-  )
-}
+  const milestonesList = [
+    {
+      title: "2020",
+      description: "Ano de Fundação",
+    },
+    {
+      title: "+1000",
+      description: "Clientes Satisfeitos",
+    },
+    {
+      title: "+R$ 200k",
+      description: "De Investimentos",
+    },
+  ];
 
-export default Milestones
+  return (
+    <Box sx={{ backgroundColor: "primary.main" }} component={"section"} py={6}>
+      <Container maxWidth={"xl"}>
+        <Stack spacing={8} alignItems={"center"}>
+          <Typography>Nossas Conquistas</Typography>
+          <List sx={{ display: "flex", flexDirection: "row", gap: 18 }}>
+            {milestonesList.map((milestone, index) => (
+              <ListItem key={index} disablePadding>
+                <ListItemText
+                  primary={milestone.title}
+                  secondary={milestone.description}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Milestones;
